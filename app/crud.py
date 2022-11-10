f319176f31c08748b66d80b62c63d435ef7b5ae1
AleksanderSzok <szokmaster@gmail.com>
2022-11-10 18:18:54 +0100

    change to async all endpoints

--- app/crud.py
+++ app/crud.py
@@ -1,23 +1,20 @@
 from sqlalchemy import desc
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
-from sqlalchemy.orm import Session
 
 from . import models
 from . import schemas
 
 
-async def get_suppliers(db: Session):
+async def get_suppliers(db: AsyncSession):
     query = select(models.Supplier).order_by(models.Supplier.SupplierID)
     return await db.execute(query)
 
 
-def get_supplier(db: Session, supplier_id: int):
-    return (
-        db.query(models.Supplier)
-        .filter(models.Supplier.SupplierID == supplier_id)
-        .one_or_none()
-    )
+async def get_supplier(db: AsyncSession, supplier_id: int):
+    query = select(models.Supplier).filter(models.Supplier.SupplierID == supplier_id)
+    result = await db.execute(query)
+    return result.scalar_one_or_none()
 
 
 async def get_product(db: AsyncSession, supplier_id: int):
@@ -46,26 +43,20 @@ def get_product_aux(query_element):
     }
 
 
-def post_supplier(db: Session, item: schemas.SupplierPost):
+async def post_supplier(db: AsyncSession, item: schemas.SupplierPost):
     db_item = models.Supplier(**item.dict())
     db.add(db_item)
-    db.commit()
-    db.refresh(db_item)
+    await db.commit()
     return db_item
 
 
-def put_supplier(db: Session, item: schemas.SupplierPost, supplier_id: int):
-    supplier = (
-        db.query(models.Supplier)
-        .filter(models.Supplier.SupplierID == supplier_id)
-        .one_or_none()
-    )
+async def put_supplier(db: AsyncSession, item: schemas.SupplierPost, supplier_id: int):
+    supplier = await get_supplier(db=db, supplier_id=supplier_id)
     if not supplier:
         return
     for k, v in item.dict().items():
         if v:
             setattr(supplier, k, v)
 
-    db.commit()
-    db.refresh(supplier)
+    await db.commit()
     return supplier
--- app/views.py
+++ app/views.py
@@ -1,25 +1,24 @@
 from typing import List
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Response
 from pydantic import PositiveInt
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import Session
 
-from . import crud, schemas, models
+from . import crud, schemas
 from .database import get_db
 
 router = APIRouter()
 
 
 @router.get("/suppliers", response_model=List[schemas.Supplier])
-async def get_suppliers(db: Session = Depends(get_db)):
+async def get_suppliers(db: AsyncSession = Depends(get_db)):
     suppliers = await crud.get_suppliers(db)
     return [supplier[0] for supplier in suppliers]
 
 
 @router.get("/suppliers/{supplier_id}", response_model=schemas.SupplierTwo)
-def get_shipper(supplier_id: PositiveInt, db: Session = Depends(get_db)):
-    db_supplier = crud.get_supplier(db, supplier_id)
+async def get_shipper(supplier_id: PositiveInt, db: AsyncSession = Depends(get_db)):
+    db_supplier = await crud.get_supplier(db, supplier_id)
     if db_supplier is None:
         raise HTTPException(status_code=404, detail="Supplier not found")
     return db_supplier
@@ -33,34 +32,33 @@ async def get_products(supplier_id: PositiveInt, db: AsyncSession = Depends(get_
     return db_products
 
 
-@router.post("/suppliers", response_model=schemas.SupplierResponse, status_code=201)
-def create_supplier(item: schemas.SupplierPost, db: Session = Depends(get_db)):
-    return crud.post_supplier(db=db, item=item)
+@router.post("/suppliers", response_model=schemas.SupplierPost, status_code=201)
+async def create_supplier(
+    item: schemas.SupplierPost, db: AsyncSession = Depends(get_db)
+):
+    return await crud.post_supplier(db=db, item=item)
 
 
 @router.put(
     "/suppliers/{supplier_id}", response_model=schemas.SupplierResponse, status_code=200
 )
-def update_supplier(
-    item: schemas.SupplierPost, supplier_id: int, db: Session = Depends(get_db)
+async def update_supplier(
+    item: schemas.SupplierPost, supplier_id: int, db: AsyncSession = Depends(get_db)
 ):
-    update_item = crud.put_supplier(db=db, item=item, supplier_id=supplier_id)
+    update_item = await crud.put_supplier(db=db, item=item, supplier_id=supplier_id)
     if not update_item:
         raise HTTPException(status_code=404)
     return update_item
 
 
-@router.delete("/suppliers/{supplier_id}", status_code=204)
-def delete_supplier(supplier_id: int, db: Session = Depends(get_db)):
-    supplier = (
-        db.query(models.Supplier)
-        .filter(models.Supplier.SupplierID == supplier_id)
-        .one_or_none()
-    )
+@router.delete("/suppliers/{supplier_id}", status_code=204, response_class=Response)
+async def delete_supplier(supplier_id: int, db: AsyncSession = Depends(get_db)):
+    supplier = await crud.get_supplier(db=db, supplier_id=supplier_id)
     if not supplier:
         raise HTTPException(status_code=404)
-    db.delete(supplier)
-    db.commit()
+    await db.delete(supplier)
+    await db.commit()
+    return
 
 
 @router.get("/")
